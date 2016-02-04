d237b1471a20a1de4ad8bf736ff264eddd37d463
Ray Vafa <vafatabar@gmail.com>
2016-02-04 14:15:36 +1100

    add some indents

--- index.ios.js
+++ index.ios.js
@@ -4,20 +4,20 @@
  */
 'use strict';
 import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+	AppRegistry,
+	Component,
+	StyleSheet,
+	Text,
+	View
+	} from 'react-native';
 var Login = require('./Login.js');
 
 class GithubBrowser extends Component {
-  render() {
-    return (
-	    <Login />
-    );
-  }
+	render() {
+		return (
+			<Login />
+		);
+	}
 }
 
 AppRegistry.registerComponent('GithubBrowser', () => GithubBrowser);
