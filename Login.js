c7f939227f6597b35245bcf78347d257a175e95a
Ray Vafa <vafatabar@gmail.com>
2016-02-04 14:11:50 +1100

    breaking Auth service to its own file

--- Login.js
+++ Login.js
@@ -75,32 +75,14 @@ class Login extends Component {
 	onLoginPressed() {
 		this.setState({showProgress: true});
 
-		var b = new buffer.Buffer(this.state.userName + ':' + this.state.password);
-		var encodedAuth = b.toString('base64');
+		var authService = require('./AuthService.js');
+		authService.login({
+			username: this.state.username,
+			password: this.state.password
+		}, (results) => {
+			this.setState(Object.assign({showProgress: false}, results));
+		});
 
-		fetch('https://api.github.com/user', {
-			headers: {
-				'Authorization': 'Basic ' + encodedAuth
-			}
-		}).then((response)=>{
-			console.log(response);
-			if(response.status >= 200 && response.status < 300){
-				return response;
-			}
-			throw {
-				badCredentials: response.status == 401,
-				unknownError: response.status != 401
-			}
-		}).then((response) => {
-				return response.json();
-		}).then((results) => {
-			console.log(results);
-			this.setState({success: true});
-		}).catch((err) => {
-			this.setState(err);
-		}).finally( () => {
-			this.setState({showProgress: false});
-		})
 	}
 };
 
